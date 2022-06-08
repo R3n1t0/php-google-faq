f809eba014805e71ec9693b87b765fee59c4add9
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-06-08 16:27:38 +0200

    array ciclata nel html

--- index.php
+++ index.php
@@ -84,6 +84,37 @@ $faqs = [
               </p>
               "
   ]
-]
-
+  ];
+var_dump($faqs)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+
+  <header>
+
+  </header>
+
+  <div class="text-container">
+    <?php foreach($faqs as $faq):?>
+      <div class="text">
+        <h2>
+          <?php echo $faq["title"]?>
+        </h2>
+        <div>
+        <?php echo $faq["body"]?>
+        </div>
+      </div>
+    <?php endforeach;?>
+  </div>
+  
+</body>
+</html>
